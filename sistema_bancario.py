4ba2de2db43a5a1823d67cf45a556e54c6708232
Leonardoassis7 <leonardorobertoassis@gmail.com>
2024-02-20 22:38:56 -0300

    Criação da função de novo usuário, listar conta, nova conta

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -56,7 +56,7 @@ def exibir_extrato(saldo, /, *, extrato):
     print("\n================ EXTRATO ================")
     print("Não foram realizadas movimentações." if not extrato else extrato)
     print(f"\nSaldo:\t\tR$ {saldo:.2f}")
-    print("==========================================")
+    print("===========================================")
 
 
 def criar_usuario(usuarios):
